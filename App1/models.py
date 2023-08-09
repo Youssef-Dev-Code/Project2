09b83839c13a3f46f83c3271e88181e2efb7bb61
Youssef-Dev-Code <youssefazaiez21@gmail.com>
2023-08-09 12:10:37 +0100

    commit

--- App1/models.py
+++ App1/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Employee:
+    def __init__(self, Name, Age, BirthDay, email, Salary):
+        self.Name = Name
+        self.Age = Age
+        self.BirthDay = BirthDay
+        self.email = email
+        self.Salary = Salary
